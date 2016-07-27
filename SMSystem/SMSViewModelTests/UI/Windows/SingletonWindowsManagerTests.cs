caa6b167497199737a7841cf325c5798eff7e191
cbqb22 <shu_masafumi@yahoo.co.jp>
2016-07-28 02:51:14 +0900

    不要なテストプロジェクトを削除＆テストプロジェクト名を修正
    SMSViewModelTests1→SMSViewModelTestsへ

--- SMSystem/SMSViewModelTests/UI/Windows/SingletonWindowsManagerTests.cs
+++ SMSystem/SMSViewModelTests/UI/Windows/SingletonWindowsManagerTests.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SMSViewModel.UI.Windows;
-using SMSView.UI.Windows;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,20 +11,12 @@ namespace SMSViewModel.UI.Windows.Tests
     [TestClass()]
     public class SingletonWindowsManagerTests
     {
-
         [TestMethod()]
         public void HasWindowTest()
         {
             var win = SingletonWindowsManager.HasWindow<BaseWindow>(typeof(BaseWindow));
-            Assert.AreEqual(null, win);
 
+            Assert.AreEqual(null,win);
         }
-
-        //[TestMethod()]
-        //public void GetorMakeWindowTest()
-        //{
-        //    var win = SingletonWindowsManager.GetorMakeWindow<Menu>(typeof(Menu));
-        //    Assert.AreEqual(SingletonWindowsManager.SingletonWindowsList[0] as Menu, win);
-        //}
     }
 }
